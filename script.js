c9b9d19c37100e19416470ea9bac90d0fe3e0f33
YashiniAnbu <yashinianbu2004@gmail.com>
2026-03-26 17:54:52 +0530

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.getElementById("userContainer");
+async function fetchUser() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+        console.log(users);
+        displayUsers(users);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+fetchUser();
+
+function displayUsers(users) {
+    users.forEach((user) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `<h3>${user.name}</h3>
+            <p><strong>Email:</strong>${user.email}</p>
+            <p><strong>Phone:</strong>${user.phone}</p>
+            <p><strong>City:</strong>${user.address.city}</p>
+            <p><strong>Company:</strong>${user.company.name}</p>`;
+        container.appendChild(card);
+    });
+}
